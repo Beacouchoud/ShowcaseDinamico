6e5d407729485f8dae9ec8820cfbe74f6e3b6e99
Bea <34965770+Beacouchoud@users.noreply.github.com>
2022-01-19 22:08:49 +0100

    Folder structure and initial approach

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//TODO: Implementar métodos
+const createShowcaseProducts = () => {}
+
+const addElementToChart = () => {}
+const addElementToChart = () => {}
+
+const increaseTotalCost = () => {}
+const decreaseTotalCost = () => {}
+
+const increaseElementStock = () => {}
+const decreaseElementStock = () => {}
+
+const emptyChart = () => {}
+
